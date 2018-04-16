29725fac9a2243d5b6bc377b375b0a3206ba6056
Phil Howard <phil@pimoroni.com>
2018-04-16 16:43:41 +0000

    Fixes for #3 and #4

--- library/neopixel/__init__.py
+++ library/neopixel/__init__.py
@@ -1,2 +1,3 @@
 # Included for backwards-compatibility with old import name
-from rpi_ws281x import __version__, PixelStrip, Adafruit_NeoPixel, Color
+from rpi_ws281x import __version__, PixelStrip, Adafruit_NeoPixel, Color, ws
+from _rpi_ws281x import *
