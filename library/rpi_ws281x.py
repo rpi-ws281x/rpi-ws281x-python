c23f472a898c2ece40bd9b79f56b90b552bfd4a9
Phil Howard <phil@pimoroni.com>
2023-04-25 14:12:00 +0100

    SWIG: Reformat Python with black.

--- library/rpi_ws281x.py
+++ library/rpi_ws281x.py
@@ -5,6 +5,7 @@
 # the SWIG interface file instead.
 
 from sys import version_info as _swig_python_version_info
+
 if _swig_python_version_info < (2, 7, 0):
     raise RuntimeError("Python 2.7 or later required")
 
@@ -19,12 +20,17 @@ try:
 except ImportError:
     import __builtin__
 
+
 def _swig_repr(self):
     try:
         strthis = "proxy of " + self.this.__repr__()
     except __builtin__.Exception:
         strthis = ""
-    return "<%s.%s; %s >" % (self.__class__.__module__, self.__class__.__name__, strthis,)
+    return "<%s.%s; %s >" % (
+        self.__class__.__module__,
+        self.__class__.__name__,
+        strthis,
+    )
 
 
 def _swig_setattr_nondynamic_instance_variable(set):
@@ -37,6 +43,7 @@ def _swig_setattr_nondynamic_instance_variable(set):
             set(self, name, value)
         else:
             raise AttributeError("You cannot add instance attributes to %s" % self)
+
     return set_instance_attr
 
 
@@ -46,18 +53,22 @@ def _swig_setattr_nondynamic_class_variable(set):
             set(cls, name, value)
         else:
             raise AttributeError("You cannot add class attributes to %s" % cls)
+
     return set_class_attr
 
 
 def _swig_add_metaclass(metaclass):
     """Class decorator for adding a metaclass to a SWIG wrapped class - a slimmed down version of six.add_metaclass"""
+
     def wrapper(cls):
         return metaclass(cls.__name__, cls.__bases__, cls.__dict__.copy())
+
     return wrapper
 
 
 class _SwigNonDynamicMeta(type):
     """Meta class to enforce nondynamic attributes (no new attributes) for a class"""
+
     __setattr__ = _swig_setattr_nondynamic_class_variable(type.__setattr__)
 
 
@@ -78,41 +89,82 @@ WS2811_STRIP_BGR = _rpi_ws281x.WS2811_STRIP_BGR
 WS2812_STRIP = _rpi_ws281x.WS2812_STRIP
 SK6812_STRIP = _rpi_ws281x.SK6812_STRIP
 SK6812W_STRIP = _rpi_ws281x.SK6812W_STRIP
+
+
 class ws2811_channel_t(object):
-    thisown = property(lambda x: x.this.own(), lambda x, v: x.this.own(v), doc="The membership flag")
+    thisown = property(
+        lambda x: x.this.own(), lambda x, v: x.this.own(v), doc="The membership flag"
+    )
     __repr__ = _swig_repr
-    gpionum = property(_rpi_ws281x.ws2811_channel_t_gpionum_get, _rpi_ws281x.ws2811_channel_t_gpionum_set)
-    invert = property(_rpi_ws281x.ws2811_channel_t_invert_get, _rpi_ws281x.ws2811_channel_t_invert_set)
-    count = property(_rpi_ws281x.ws2811_channel_t_count_get, _rpi_ws281x.ws2811_channel_t_count_set)
-    strip_type = property(_rpi_ws281x.ws2811_channel_t_strip_type_get, _rpi_ws281x.ws2811_channel_t_strip_type_set)
-    leds = property(_rpi_ws281x.ws2811_channel_t_leds_get, _rpi_ws281x.ws2811_channel_t_leds_set)
-    brightness = property(_rpi_ws281x.ws2811_channel_t_brightness_get, _rpi_ws281x.ws2811_channel_t_brightness_set)
-    wshift = property(_rpi_ws281x.ws2811_channel_t_wshift_get, _rpi_ws281x.ws2811_channel_t_wshift_set)
-    rshift = property(_rpi_ws281x.ws2811_channel_t_rshift_get, _rpi_ws281x.ws2811_channel_t_rshift_set)
-    gshift = property(_rpi_ws281x.ws2811_channel_t_gshift_get, _rpi_ws281x.ws2811_channel_t_gshift_set)
-    bshift = property(_rpi_ws281x.ws2811_channel_t_bshift_get, _rpi_ws281x.ws2811_channel_t_bshift_set)
-    gamma = property(_rpi_ws281x.ws2811_channel_t_gamma_get, _rpi_ws281x.ws2811_channel_t_gamma_set)
+    gpionum = property(
+        _rpi_ws281x.ws2811_channel_t_gpionum_get,
+        _rpi_ws281x.ws2811_channel_t_gpionum_set,
+    )
+    invert = property(
+        _rpi_ws281x.ws2811_channel_t_invert_get, _rpi_ws281x.ws2811_channel_t_invert_set
+    )
+    count = property(
+        _rpi_ws281x.ws2811_channel_t_count_get, _rpi_ws281x.ws2811_channel_t_count_set
+    )
+    strip_type = property(
+        _rpi_ws281x.ws2811_channel_t_strip_type_get,
+        _rpi_ws281x.ws2811_channel_t_strip_type_set,
+    )
+    leds = property(
+        _rpi_ws281x.ws2811_channel_t_leds_get, _rpi_ws281x.ws2811_channel_t_leds_set
+    )
+    brightness = property(
+        _rpi_ws281x.ws2811_channel_t_brightness_get,
+        _rpi_ws281x.ws2811_channel_t_brightness_set,
+    )
+    wshift = property(
+        _rpi_ws281x.ws2811_channel_t_wshift_get, _rpi_ws281x.ws2811_channel_t_wshift_set
+    )
+    rshift = property(
+        _rpi_ws281x.ws2811_channel_t_rshift_get, _rpi_ws281x.ws2811_channel_t_rshift_set
+    )
+    gshift = property(
+        _rpi_ws281x.ws2811_channel_t_gshift_get, _rpi_ws281x.ws2811_channel_t_gshift_set
+    )
+    bshift = property(
+        _rpi_ws281x.ws2811_channel_t_bshift_get, _rpi_ws281x.ws2811_channel_t_bshift_set
+    )
+    gamma = property(
+        _rpi_ws281x.ws2811_channel_t_gamma_get, _rpi_ws281x.ws2811_channel_t_gamma_set
+    )
 
     def __init__(self):
         _rpi_ws281x.ws2811_channel_t_swiginit(self, _rpi_ws281x.new_ws2811_channel_t())
+
     __swig_destroy__ = _rpi_ws281x.delete_ws2811_channel_t
+
 
 # Register ws2811_channel_t in _rpi_ws281x:
 _rpi_ws281x.ws2811_channel_t_swigregister(ws2811_channel_t)
 
+
 class ws2811_t(object):
-    thisown = property(lambda x: x.this.own(), lambda x, v: x.this.own(v), doc="The membership flag")
+    thisown = property(
+        lambda x: x.this.own(), lambda x, v: x.this.own(v), doc="The membership flag"
+    )
     __repr__ = _swig_repr
-    render_wait_time = property(_rpi_ws281x.ws2811_t_render_wait_time_get, _rpi_ws281x.ws2811_t_render_wait_time_set)
+    render_wait_time = property(
+        _rpi_ws281x.ws2811_t_render_wait_time_get,
+        _rpi_ws281x.ws2811_t_render_wait_time_set,
+    )
     device = property(_rpi_ws281x.ws2811_t_device_get, _rpi_ws281x.ws2811_t_device_set)
     rpi_hw = property(_rpi_ws281x.ws2811_t_rpi_hw_get, _rpi_ws281x.ws2811_t_rpi_hw_set)
     freq = property(_rpi_ws281x.ws2811_t_freq_get, _rpi_ws281x.ws2811_t_freq_set)
     dmanum = property(_rpi_ws281x.ws2811_t_dmanum_get, _rpi_ws281x.ws2811_t_dmanum_set)
-    channel = property(_rpi_ws281x.ws2811_t_channel_get, _rpi_ws281x.ws2811_t_channel_set)
+    channel = property(
+        _rpi_ws281x.ws2811_t_channel_get, _rpi_ws281x.ws2811_t_channel_set
+    )
 
     def __init__(self):
         _rpi_ws281x.ws2811_t_swiginit(self, _rpi_ws281x.new_ws2811_t())
+
     __swig_destroy__ = _rpi_ws281x.delete_ws2811_t
+
 
 # Register ws2811_t in _rpi_ws281x:
 _rpi_ws281x.ws2811_t_swigregister(ws2811_t)
@@ -134,31 +186,38 @@ WS2811_ERROR_SPI_SETUP = _rpi_ws281x.WS2811_ERROR_SPI_SETUP
 WS2811_ERROR_SPI_TRANSFER = _rpi_ws281x.WS2811_ERROR_SPI_TRANSFER
 WS2811_RETURN_STATE_COUNT = _rpi_ws281x.WS2811_RETURN_STATE_COUNT
 
+
 def ws2811_init(ws2811):
     return _rpi_ws281x.ws2811_init(ws2811)
+
 
 def ws2811_fini(ws2811):
     return _rpi_ws281x.ws2811_fini(ws2811)
 
+
 def ws2811_render(ws2811):
     return _rpi_ws281x.ws2811_render(ws2811)
+
 
 def ws2811_wait(ws2811):
     return _rpi_ws281x.ws2811_wait(ws2811)
 
+
 def ws2811_get_return_t_str(state):
     return _rpi_ws281x.ws2811_get_return_t_str(state)
+
 
 def ws2811_set_custom_gamma_factor(ws2811, gamma_factor):
     return _rpi_ws281x.ws2811_set_custom_gamma_factor(ws2811, gamma_factor)
 
+
 def ws2811_led_get(channel, lednum):
     return _rpi_ws281x.ws2811_led_get(channel, lednum)
+
 
 def ws2811_led_set(channel, lednum, color):
     return _rpi_ws281x.ws2811_led_set(channel, lednum, color)
 
+
 def ws2811_channel_get(ws, channelnum):
     return _rpi_ws281x.ws2811_channel_get(ws, channelnum)
-
-
